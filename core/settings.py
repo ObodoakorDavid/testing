9365ea3793731829391bd034322fa684a7c81b03
ObodoakorDavid <obodoakordavid@outlook.com>
2022-10-29 19:28:53 +0100

    zz commit

--- core/settings.py
+++ core/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+# from decouple import config
 
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://newtodo007.herokuapp.com/']
 
